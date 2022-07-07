b73b613643d7b307b2f8bfb097f9d3cb2a2c959f
GHTeodor <Theodor.Lviv@gmail.com>
2022-07-07 20:07:28 +0300

    +DTO

--- src/main/java/com/springboot/javaspringboot/models/Passport.java
+++ src/main/java/com/springboot/javaspringboot/models/Passport.java
@@ -1,20 +1,22 @@
 package com.springboot.javaspringboot.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Passport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private boolean biometric;
     @OneToOne(mappedBy = "passport", cascade = CascadeType.ALL)
+    @JsonIgnore()
     private User user;
 }
